a6d0c8b5229a4408328f7efef120dca2d493cf8d
wendellmoraisz <wendellmorais3319@gmail.com>
2023-09-16 16:32:58 -0300

    Add Entity Framework

---
+++ NewsScrapingMonolithic.Persistence/Context/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using NewsScrapingMonolithic.Domain.Entities;
+
+namespace NewsScrapingMonolithic.Persistence.Context;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Email> Emails { get; set; }
+}
