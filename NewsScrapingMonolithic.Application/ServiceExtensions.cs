833cb1cbdc70c8fd40b96ec567e857f3f34c18b3
wendellmoraisz <wendellmorais3319@gmail.com>
2023-09-16 16:04:56 -0300

    add service extensions

---
+++ NewsScrapingMonolithic.Application/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace NewsScrapingMonolithic.Application;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+    }
+}
