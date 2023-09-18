3df9483dd9e62b02c3137f4b3d5afc3ca01f2369
wendellmoraisz <wendellmorais3319@gmail.com>
2023-09-18 09:03:21 -0300

    initialize setup

--- NewsScrapingMonolithic.WebAPI/Program.cs
+++ NewsScrapingMonolithic.WebAPI/Program.cs
@@ -1,11 +1,33 @@
+using NewsScrapingMonolithic.Application;
+using NewsScrapingMonolithic.Persistence;
+using NewsScrapingMonolithic.Persistence.Context;
+using NewsScrapingMonolithic.WebAPI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigurePersistence(builder.Configuration);
+builder.Services.ConfigureApplication();
+
+builder.Services.ConfigureApiBehavior();
+builder.Services.ConfigureCorsPolicy();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var serviceScope = app.Services.CreateScope();
+var dataContext = serviceScope.ServiceProvider.GetService<DataContext>();
+dataContext?.Database.EnsureCreated();
+
+if (app.Environment.IsDevelopment())
+{ 
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseErrorHandler();
+app.UseCors();
 app.MapControllers();
-
-app.MapGet("/", () => "Hello World!");
-
 app.Run();
