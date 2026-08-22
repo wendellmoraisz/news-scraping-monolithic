7f0c7a3c4910ec01f2f9b03a1aaaf8ea4f1dba3b
Wendell <wendellmorais.dev@gmail.com>
2026-08-22 20:15:16 -0300

    refactor: remove hardcoded hosts and get by db

--- NewsScrapingMonolithic.Application/UseCases/SendScrapedNews/SendScrapedNews.cs
+++ NewsScrapingMonolithic.Application/UseCases/SendScrapedNews/SendScrapedNews.cs
@@ -9,6 +9,7 @@ public sealed class SendScrapedNews
     private readonly IScrapingService _scrapingService;
     private readonly INewsRepository _newsRepository;
     private readonly IEmailRepository _emailRepository;
+    private readonly INewsPageRepository _newsPageRepository;
     private readonly IUnityOfWork _unityOfWork;
 
     public SendScrapedNews(
@@ -16,6 +17,7 @@ public sealed class SendScrapedNews
         IScrapingService scrapingService,
         INewsRepository newsRepository,
         IEmailRepository emailRepository,
+        INewsPageRepository newsPageRepository,
         IUnityOfWork unityOfWork
         )
     {
@@ -23,21 +25,28 @@ public sealed class SendScrapedNews
         _scrapingService = scrapingService;
         _newsRepository = newsRepository;
         _emailRepository = emailRepository;
+        _newsPageRepository = newsPageRepository;
         _unityOfWork = unityOfWork;
     }
 
-    public async Task Execute(string baseUrl, string host, CancellationToken cancellationToken)
+    public async Task Execute(CancellationToken cancellationToken)
     {
-        var newsList = await _scrapingService.ExtractNews(baseUrl, host);
-        var emailsList = await _emailRepository.GetAll(cancellationToken);
+        var newsPages = await _newsPageRepository.GetAll(cancellationToken);
 
-        foreach (var news in newsList)
+        foreach (var newsPage in newsPages)
         {
-            var newsIsAlreadySent = await _newsRepository.GetByTitle(news.Title, cancellationToken) != null;
-            if (newsIsAlreadySent) continue;
-            _newsRepository.Create(news);
-            await _unityOfWork.Save(cancellationToken);
-            await _emailService.Send(emailsList, news.Title, news.Content);
+            var newsList = await _scrapingService.ExtractNews(newsPage.Url, newsPage.HeaderHost);
+            var emailsList = await _emailRepository.GetByHost(newsPage.Url, cancellationToken);
+
+            foreach (var news in newsList)
+            {
+                var newsIsAlreadySent = await _newsRepository.GetByTitle(news.Title, cancellationToken) != null;
+                if (newsIsAlreadySent) continue;
+
+                _newsRepository.Create(news);
+                await _unityOfWork.Save(cancellationToken);
+                await _emailService.Send(emailsList, news.Title, news.Content);
+            }
         }
     }
 }
--- NewsScrapingMonolithic.WebAPI/ScheduledJobs/NewsScrapingScheduler.cs
+++ NewsScrapingMonolithic.WebAPI/ScheduledJobs/NewsScrapingScheduler.cs
@@ -14,8 +14,7 @@ public class NewsScrapingScheduler : IJob
 
     public async Task Execute(IJobExecutionContext context)
     {
-        await _sendScrapedNews.Execute("https://altamira.ifpa.edu.br", "altamira.ifpa.edu.br", CancellationToken.None);
-        await _sendScrapedNews.Execute("https://ifpa.edu.br", "ifpa.edu.br", CancellationToken.None);
+        await _sendScrapedNews.Execute(context.CancellationToken);
     }
 
 }
